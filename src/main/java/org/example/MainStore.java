3f5c2f2028bf260d105a449c402ed6218dab4557
Elvis Ishimwe <Eishimwe@appdev.yearup.org>
2023-04-28 14:17:27 -0500

    keeping my works

---
+++ src/main/java/org/example/MainStore.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.Map;
+
+
+public class MainStore {
+    public static void main(String[] args)
+    {
+        Map<String, Product> inventory;
+        List<Product> cart;
+    }
+}
