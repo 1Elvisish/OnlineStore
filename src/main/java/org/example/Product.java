b0972799acfcfb77df7ae4312c07a65676761479
Elvis Ishimwe <Eishimwe@appdev.yearup.org>
2023-04-29 11:10:29 -0500

    keeping my works

--- src/main/java/org/example/Product.java
+++ src/main/java/org/example/Product.java
@@ -1,4 +1,74 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
 public class Product {
-}
+    private static Store store;
+    private static ArrayList<Store> cart;
+    private static double total;
+
+    public static void main(String[] args) {
+        store = new Store();
+        cart = new ArrayList<>();
+        total = 0.0;
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("=== Store Home Screen ===");
+            System.out.println("1. Show Products");
+            System.out.println("2. Show Cart");
+            System.out.println("3. Exit");
+
+            int choice = scanner.nextInt();
+
+            switch (choice) {
+                case 1:
+                    showProducts(scanner);
+                    break;
+                case 2:
+                    showCart(scanner);
+                    break;
+                case 3:
+                    System.out.println("Thank you for shopping with us!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice! Please try again.");
+                    break;
+            }
+        }
+    }
+
+    private static void showProducts(Scanner scanner) {
+        System.out.println("=== Products ===");
+        for (Store product : store.getInventory()) {
+            System.out.println(product);
+        }
+
+        while (true) {
+            System.out.print("Enter the product id to add to cart or X to go back: ");
+            String id = scanner.next();
+
+            if (id.equalsIgnoreCase("X")) {
+                break;
+            }
+
+            Store product = store.getProductById(id);
+
+            if (product == null) {
+                System.out.println("Invalid product id! Please try again.");
+            } else {
+                cart.add(product);
+                System.out.println(product.getName() + " added to cart.");
+            }
+        }
+    }
+
+    private static void showCart(Scanner scanner) {
+        System.out.println("=== Cart ===");
+        for (Store product);
+
+
+
---
+++ src/main/java/org/example/Store.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+public class Store {
+    private ArrayList<Store> inventory;
+    public Store() {
+        inventory = new ArrayList<>();
+        loadInventory();
+    }
+    private void loadInventory() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("inventory.csv"));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] fields = line.split("\\|");
+                String id = fields[0];
+                String name = fields[1];
+                double price = Double.parseDouble(fields[2]);
+                Store product = new Store();
+                inventory.add(product);
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
